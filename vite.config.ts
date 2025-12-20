9dd0f8a1a082fdaddeab236a79f7bf7ee2f7c0ae
dhashwinkennedy-cmd <dhashwinkennedy@gmail.com>
2025-12-20 21:32:10 +0530

    deploy check 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,8 +5,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 3000
   }
 });
